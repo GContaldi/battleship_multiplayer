9f66d809e1f8fcd04dfa3aa0ef06b955209faa4d
Gaetano Contaldi <gaetanocontaldi000@gmail.com>
2015-09-21 10:38:15 +0200

    improve lint

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,10 @@
-var webpack = require('webpack');
-
 module.exports = function(config) {
   config.set({
-    browsers: ['PhantomJS'], //run in Chrome, PhantomJS, Firefox
-    frameworks: ['jasmine'], //use the mocha test framework
+    browsers: ['PhantomJS'], // run in Chrome, PhantomJS, Firefox
+    frameworks: ['jasmine'], // use the mocha test framework
     files: [
-      './node_modules/phantomjs-polyfill/bind-polyfill.js', //bind polyfill for phantomjs
-      'tests.webpack.js', //just load this file
+      './node_modules/phantomjs-polyfill/bind-polyfill.js', // bind polyfill for phantomjs
+      'tests.webpack.js', // just load this file
       {
         pattern: 'resources/**/*.js',
         included: false
@@ -17,17 +15,17 @@ module.exports = function(config) {
       }
     ],
     preprocessors: {
-      'resources/**/*.js': [ 'eslint' ],
-      'specs/**/*.js': [ 'eslint' ],
-      'tests.webpack.js': ['webpack', 'sourcemap'] //preprocess with webpack and our sourcemap loader
+      'resources/**/*.js': ['eslint'],
+      'specs/**/*.js': ['eslint'],
+      'tests.webpack.js': ['webpack', 'sourcemap'] // preprocess with webpack and our sourcemap loader
     },
-    reporters: ['dots'], //report results in this format
+    reporters: ['dots'], // report results in this format
     eslint: {
       stopOnError: true,
       stopOnWarning: false
     },
-    webpack: { //kind of a copy of your webpack config
-      devtool: 'inline-source-map', //just do inline source maps instead of the default
+    webpack: { // kind of a copy of your webpack config
+      devtool: 'inline-source-map', // just do inline source maps instead of the default
       module: {
         loaders: [
           {
@@ -38,7 +36,7 @@ module.exports = function(config) {
       }
     },
     webpackServer: {
-      noInfo: true //please don't spam the console when running in karma!
+      noInfo: true // please don't spam the console when running in karma!
     }
   });
 };
--- tests.webpack.js
+++ tests.webpack.js
@@ -1,3 +1,3 @@
-var context = require.context('./specs', true, /Spec\.js$/); //make sure you have your directory and regex test set correctly!
+var context = require.context('./specs', true, /Spec\.js$/); // make sure you have your directory and regex test set correctly!
 context.keys().forEach(context);
 console.log(context.keys());
